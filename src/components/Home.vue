0d00841d069b312e3310e7b8280b7f02a019258c
Sylvain <sylvain.denyse@gmail.com>
2018-05-24 20:07:36 +0200

    init project

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container grid-list-xl>
+      <v-layout v-bind="binding">
+        <v-flex md5>
+          <v-card>
+            <v-card-title>Create show</v-card-title>
+            <v-btn>Click</v-btn>
+          </v-card>
+        </v-flex>
+        <v-flex md5>
+          <v-card>
+            <v-card-title>Edit show</v-card-title>
+            <v-btn>Click</v-btn>
+          </v-card>
+        </v-flex>
+        <v-flex md5>
+          <v-card>
+            <v-card-title>Launch show</v-card-title>
+            <v-btn>Click</v-btn>
+          </v-card>
+        </v-flex>
+      </v-layout>
+  </v-container>
+</template>
