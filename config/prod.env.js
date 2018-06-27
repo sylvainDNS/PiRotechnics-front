770cf54b61a1f57974228a25f5b2153c6f8d8273
Sylvain <sylvain.denyse@gmail.com>
2018-06-27 10:42:28 +0200

    check env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"http://localhost"',
-  API_PORT: '"4444"'
+  API_URL: '"https://piroapi.sylvaindenyse.me"',
+  API_PORT: '"443"'
 }
