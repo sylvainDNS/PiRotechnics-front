fad6b9b43467381a6fbad3edb39bf027106d8cb6
Sylvain <sylvain.denyse@gmail.com>
2018-07-02 22:32:51 +0200

    update api url

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://piroapi.sylvaindenyse.me"',
+  API_URL: '"https://api.pirotechnics.sylvaindenyse.me"',
   API_PORT: '"443"'
 }
