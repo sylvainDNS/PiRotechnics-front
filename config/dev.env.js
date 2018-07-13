febd43dcbfb322eee9b5c9408a9d7d889bfff5f7
SylvainDNS <sylvain.denyse@gmail.com>
2018-07-13 14:36:52 +0200

    setup env for fucking firm proxy

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,9 +1,14 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
+'use strict';
+const merge = require('webpack-merge');
+const prodEnv = require('./prod.env');
+const env = require('../.env');
 
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_URL: '"http://localhost"',
-  API_PORT: '"4444"'
-})
+module.exports = merge(
+  prodEnv,
+  {
+    NODE_ENV: '"development"',
+    API_URL: '"http://localhost"',
+    API_PORT: '"4444"'
+  },
+  env
+);
--- config/index.js
+++ config/index.js
@@ -1,12 +1,11 @@
-'use strict'
+'use strict';
 // Template version: 1.2.8
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -20,7 +19,6 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-
     /**
      * Source Maps
      */
@@ -33,7 +31,7 @@ module.exports = {
     // https://vue-loader.vuejs.org/en/options.html#cachebusting
     cacheBusting: true,
 
-    cssSourceMap: true,
+    cssSourceMap: true
   },
 
   build: {
@@ -66,4 +64,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   }
-}
+};
