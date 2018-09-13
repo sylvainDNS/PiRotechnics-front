bde134731d253b4a4516e08ea5c98b19cb5d4ebc
Netoun <coulonnier.nicolas.44@gmail.com>
2018-09-13 14:48:24 +0200

    refonte addStep for my friend the best of diabolique

--- src/components/AddStep.vue
+++ src/components/AddStep.vue
@@ -1,26 +1,31 @@
 <template>
-  <v-layout row justify-center>
-    <v-dialog v-model="dialog" persistent max-width="500px">
-      <v-card>
-        <v-flex>
-          <v-icon x-large @click="" color="accent">add_circle_outline</v-icon>
-          <v-text-field class="time-field" light readonly solo :value="this.seconds"></v-text-field>
-          <v-icon x-large @click="" color="accent">remove_circle_outline</v-icon>
+  <v-dialog v-model="dialog" persistent max-width="250px">
+    <v-card class="show">
+      <v-layout row wrap justify-center>
+
+        <v-flex xs4 justify-center align-center>
+          <v-icon class="icon " @click=" " dark>add</v-icon>
+          <v-text-field class="time-field " light readonly solo :value="this.seconds "></v-text-field>
+          <v-icon class="icon " @click=" " dark>remove</v-icon>
         </v-flex>
-        <v-flex>
-          <v-icon x-large @click="" color="accent">add_circle_outline</v-icon>
-          <v-text-field class="time-field" light readonly solo :value="this.seconds"></v-text-field>
-          <v-icon x-large @click="" color="accent">remove_circle_outline</v-icon>
+        <v-flex xs4 justify-center align-center>
+          <v-icon class="icon " @click=" " dark>add</v-icon>
+          <v-text-field class="time-field " mask="## " light readonly solo :value="this.seconds "></v-text-field>
+          <v-icon class="icon " @click=" " dark>remove</v-icon>
         </v-flex>
-        <v-btn color="secondary" @click="clean" title="Annuler l'étape">
-          Annuler
-        </v-btn>
-        <v-btn @click="submit" title="Ajouter l'étape">
-          Ajouter
-        </v-btn>
-      </v-card>
-    </v-dialog>
-  </v-layout>
+
+        <v-flex xs12>
+          <v-btn @click="clean" title="Annuler l'étape">
+            Annuler
+          </v-btn>
+          <v-btn color="secondary " @click="submit" title="Ajouter l'étape">
+            Ajouter
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-card>
+  </v-dialog>
+
 </template>
 
 <script>
@@ -63,16 +68,22 @@ export default {
 </script>
 
 <style scoped>
-.frame {
-  padding: 0 10px 10px 10px;
+.show {
+  padding: 10px;
 }
 
 .time-field {
+  text-align: center;
+  height: 50px;
   width: 70px;
 }
 
 .incrementer {
   width: 50px;
   height: 50px;
+}
+
+.icon {
+  margin-left: 20px;
 }
 </style>
